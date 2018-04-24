93fd014e7c030f69d99793c86f267911f45be94a
FLAVIO HENRIQUE ROCHA FERREIRA <flavio.hrferreira@senacsp.edu.br>
2018-04-24 12:31:21 -0300

    parte 2/final

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace CalculadoraCPF
     {
         static void Main(string[] args)
         {
-        string cpfCliente, cpf9, cpf10;
+        string cpfCliente, cpf9, cpf10, cpfFinal;
         int contagem1=10, contagem2=11, resto=0, rs1=0, rs2=0;
 
             cpfCliente = "28591034813";
@@ -22,7 +22,17 @@ namespace CalculadoraCPF
         else
             cpf10 = cpf9 + (11-resto);
 
-        
+        for(int i = 0; i < cpf10.Length; i++)
+        {
+            rs2 += int.Parse(cpf10[i].ToString())*contagem2--;    
+        }        
+            resto = rs2 % 11;
+        if(resto < 2)
+            cpfFinal = cpf10 + 0;
+        else
+            cpfFinal = cpf10+(11-resto); 
+    
         }
+
     }
 }
