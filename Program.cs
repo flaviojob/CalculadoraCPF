647369d9964dad8cf404796b005f2d12de813118
FLAVIO HENRIQUE ROCHA FERREIRA <flavio.hrferreira@senacsp.edu.br>
2018-04-24 11:21:48 -0300

    Estrutura e Variáveis

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalculadoraCPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        string cpfCliente, cpf9, cpf10;
+        int contagem1=10, contagem2=11, resto=0, rs1=0, rs2=0;
+        
+        
+        }
+    }
+}
